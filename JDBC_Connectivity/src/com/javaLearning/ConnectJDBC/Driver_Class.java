87009c292e76d212642d8009f8d49fe8fb7a6f56
Meena Priyadharshini S R <83598565+MEENA-2@users.noreply.github.com>
2023-02-14 13:06:12 +0530

    First Commit

---
+++ JDBC_Connectivity/src/com/javaLearning/ConnectJDBC/Driver_Class.java
@@ -0,0 +1,14 @@
+package com.javaLearning.ConnectJDBC;
+
+import java.sql.SQLException;
+
+public class Driver_Class {
+
+	/* adding the throws declaration with main method to handle exception */
+	public static void main(String[] args) throws NullPointerException, SQLException {
+		
+		//creating object for student class(blueprint of the system) 
+		Student student = new Student();
+	}
+
+}
